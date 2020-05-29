94958b3158a1c4db91c1306c3d5a96e7f20a13a1
sharmadeep698 <64363262+sharmadeep698@users.noreply.github.com>
2020-05-29 21:06:22 +0545

    logo added

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const click = document.querySelector('.button');
 console.log(click)
 const input = document.getElementById("input")
 console.dir(input)
-	input.addEventListener("change", (event)=>{
+	input.addEventListener("change" || "keydown", (event)=>{
 	let name =  event.target.value
-	let num = Math.floor(Math.random() * 10);
-	body.innerHTML =(`this is the number ${num}`)
+	console.log(name)
+	let num = Math.floor(Math.random() * 6);
+	body.innerHTML =(`${name} lucky day is :  ${weekday[num]}`)
 });
+	var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
 
